c24f91f6060608fa39bf75c920e37a7e5ead4284
Marvin Truong <khatruong06@gmail.com>
2025-12-23 22:19:15 +1000

    feat: move router to separated folder

--- src/rag-controller/app/main.py
+++ src/rag-controller/app/main.py
@@ -1,19 +1,9 @@
 from fastapi import FastAPI
-from .dtos.chat_dto import ChatDto
 
+from .routers.chat_router import router as chat_router
+from .routers.helper_router import router as helper_router
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"status": "200 OK"}
-
-@app.post("/")
-def chat(dto: ChatDto):
-    return {
-        "history": dto.history,
-        "received_role": dto.role,
-        "received_content": dto.content,
-        "history_length": len(dto.history)
-    }
+app.include_router(chat_router)
+app.include_router(helper_router)
